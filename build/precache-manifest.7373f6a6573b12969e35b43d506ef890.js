cb5881f1911b1653dbe0953a49b726c63c06660c
sylvsin <lubwimi@gmail.com>
2020-07-23 15:36:54 +0200

    Created build for deploying

---
+++ build/precache-manifest.7373f6a6573b12969e35b43d506ef890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af73f9f82f752eca827c4512ccc387ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bfaeeb155b5ef7845fb",
+    "url": "/static/css/main.c0152ceb.chunk.css"
+  },
+  {
+    "revision": "9f89469a0c965744316e",
+    "url": "/static/js/2.58e37826.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.58e37826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfaeeb155b5ef7845fb",
+    "url": "/static/js/main.edea6d91.chunk.js"
+  },
+  {
+    "revision": "f24f8acb64c9645eefe9",
+    "url": "/static/js/runtime-main.33a7e51a.js"
+  }
+]);
